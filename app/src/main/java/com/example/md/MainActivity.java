051e2465d1333b2255f7671a475f0dc7a0c24b07
崔文举 <1755140651@qq.com>
2019-09-06 15:43:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/md/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.md;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.jaeger.library.StatusBarUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    private ListView lv;
+    private Integer[] img = {R.color.red, R.color.pink, R.color.purple, R.color.deep_purple, R.color.indigo, R.color.blue, R.color.light_blue, R.color.cyan,
+            R.color.teal, R.color.green, R.color.light_green, R.color.lime, R.color.yellow, R.color.amber, R.color.orange, R.color.deep_orangr, R.color.brown,
+            R.color.grey, R.color.blue_grey};
+    private String[] title = {"红色/Red", "粉色/Pink", "紫色/purple", "深紫/Deep Purple", "靛蓝/Indigo", "蓝色/Blue", "亮蓝/Light Blue", "青色/Cyan",
+            "鸭绿/Teal", "绿色/Green", "亮绿/Light Green", "酸橙/Lime", "黄色/Yello", "琥珀/Amber", "橙色/Orange", "暗橙/Deep Orange", "棕色/Brown",
+            "灰色/grey", "蓝灰/Blue Grey"};
+    private String[] colorValue = {"#F44336", "#E91E63", "#8E24AA", "#6A37B7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
+            "#8BC34A", "#CDDC39", "#FFEB3B", "#FFB300", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //设置状态栏透明度（0-255,0表示全透明，255表示不透明，不透明也就是黑色）
+        StatusBarUtil.setTranslucent(this, 0);
+        //获取组件
+        lv = findViewById(R.id.lv);
+        //生成适配器
+        SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.item_lv, new String[]{"img", "title", "colorValue"},
+                new int[]{R.id.iv_bg, R.id.tv_title, R.id.tv_colorValue});
+        lv.setAdapter(adapter);
+        //获取监听
+        lv.setOnItemClickListener(this);
+    }
+
+    //遍历数据
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < img.length; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", img[i]);
+            map.put("title", title[i]);
+            map.put("colorValue", colorValue[i]);
+            list.add(map);
+        }
+        return list;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (i == 0) {
+            startActivity(new Intent(this, Red.class));
+        }
+        if (i == 1) {
+            startActivity(new Intent(this, Pink.class));
+        }
+        if (i == 2) {
+            startActivity(new Intent(this, Purple.class));
+        }
+        if (i == 3) {
+            startActivity(new Intent(this, DeepPurple.class));
+        }
+        if (i == 4) {
+            startActivity(new Intent(this, Indigo.class));
+        }
+        if (i == 5) {
+            startActivity(new Intent(this, Blue.class));
+        }
+        if (i == 6) {
+            startActivity(new Intent(this, LightBlue.class));
+        }
+        if (i == 7) {
+            startActivity(new Intent(this, Cyan.class));
+        }
+        if (i == 8) {
+            startActivity(new Intent(this, Teal.class));
+        }
+        if (i == 9) {
+            startActivity(new Intent(this, Green.class));
+        }
+        if (i == 10) {
+            startActivity(new Intent(this, LightGreen.class));
+        }
+        if (i == 11) {
+            startActivity(new Intent(this, Lime.class));
+        }
+        if (i == 12) {
+            startActivity(new Intent(this, Yellow.class));
+        }
+        if (i == 13) {
+            startActivity(new Intent(this, Amber.class));
+        }
+        if (i == 14) {
+            startActivity(new Intent(this, Orange.class));
+        }
+        if (i == 15) {
+            startActivity(new Intent(this, DeepOrange.class));
+        }
+        if (i == 16) {
+            startActivity(new Intent(this, Brown.class));
+        }
+        if (i == 17) {
+            startActivity(new Intent(this, Grey.class));
+        }
+        if (i == 18) {
+            startActivity(new Intent(this, BlueGrey.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/md/Red.java
@@ -0,0 +1,167 @@
+package com.example.md;
+
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.jaeger.library.StatusBarUtil;
+
+public class Red extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_red50, btn_red100, btn_red200, btn_red300, btn_red400, btn_red500, btn_red600, btn_red700, btn_red800, btn_red900, btn_reda100,
+            btn_reda200, btn_reda400, btn_reda700;
+    private Intent intent;
+    private ClipboardManager cm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red);
+        //设置状态栏透明度（0-255,0表示全透明，255表示不透明，不透明也就是黑色）
+        StatusBarUtil.setTranslucent(this, 0);
+        //获取组件
+        btn_red50 = findViewById(R.id.btn_red50);
+        btn_red100 = findViewById(R.id.btn_red100);
+        btn_red200 = findViewById(R.id.btn_red200);
+        btn_red300 = findViewById(R.id.btn_red300);
+        btn_red400 = findViewById(R.id.btn_red400);
+        btn_red500 = findViewById(R.id.btn_red500);
+        btn_red600 = findViewById(R.id.btn_red600);
+        btn_red700 = findViewById(R.id.btn_red700);
+        btn_red800 = findViewById(R.id.btn_red800);
+        btn_red900 = findViewById(R.id.btn_red900);
+        btn_reda100 = findViewById(R.id.btn_reda100);
+        btn_reda200 = findViewById(R.id.btn_reda200);
+        btn_reda400 = findViewById(R.id.btn_reda400);
+        btn_reda700 = findViewById(R.id.btn_reda700);
+        //获取监听
+        btn_red50.setOnClickListener(this);
+        btn_red100.setOnClickListener(this);
+        btn_red200.setOnClickListener(this);
+        btn_red300.setOnClickListener(this);
+        btn_red400.setOnClickListener(this);
+        btn_red500.setOnClickListener(this);
+        btn_red600.setOnClickListener(this);
+        btn_red700.setOnClickListener(this);
+        btn_red800.setOnClickListener(this);
+        btn_red900.setOnClickListener(this);
+        btn_reda100.setOnClickListener(this);
+        btn_reda200.setOnClickListener(this);
+        btn_reda400.setOnClickListener(this);
+        btn_reda700.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_red50:
+               /* cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                // 将文本内容放到系统剪贴板里。
+                cm.setText(btn_red50.getText());
+                Toast.makeText(this, btn_red50.getText() + "复制成功", Toast.LENGTH_SHORT).show();*/
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red50.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red100:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red100.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red200:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red200.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red300:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red300.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red400:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red400.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red500:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red500.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red600:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red600.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red700:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red700.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red800:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red800.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_red900:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_red900.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_reda100:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_reda100.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_reda200:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_reda200.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_reda400:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_reda400.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            case R.id.btn_reda700:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, btn_reda700.getText());//注意：这里只是分享文本内容
+                intent.setType("text/*");
+                startActivity(Intent.createChooser(intent, "分享到"));
+                break;
+            default:
+                break;
+        }
+    }
+}
